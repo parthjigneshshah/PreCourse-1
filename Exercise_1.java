c75f570f6bece4ee1601ca03c8c7f09034ac4388
Parth Shah <parth.jignesh.shah@gmail.com>
2025-09-29 21:25:44 -0700

    Changes committed.

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,40 +1,52 @@
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
-    { 
-        //Write your code here
-        return false;
+    {
+        return top == -1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
-        return false;
+        if(a[top] >=MAX-1) {
+            return false;
+        }
+        else{
+            a[++top] = x;
+            return true;
+        }
+
+
     } 
   
     int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-        return -1;
-
+    {
+        if(isEmpty())
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else
+        {
+            return a[top--];
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
-        return 0;
+        if(isEmpty())
+            return 0;
+        else
+            return a[top];
     } 
 } 
   
@@ -45,7 +57,7 @@ class Main {
         Stack s = new Stack(); 
         s.push(10); 
         s.push(20); 
-        s.push(30); 
+        s.push(30);
         System.out.println(s.pop() + " Popped from stack"); 
     } 
 }
